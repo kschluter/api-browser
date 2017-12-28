c4cf8923def5394031d167e4ba8136bfdf0e9323
Kory Schluter <kschluter@southbaymedia.com>
2017-12-28 09:47:34 -0800

    Add main functionality

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,118 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <el-container>
+        <el-header><h1>API Browser</h1></el-header>
+        <hr />
+        <el-main>
+            <data-tables :data='tableData' :actions-def='getActionsDef()' :action-col-def="actionColDef" :checkbox-filter-def='getCheckFilterDef()'>
+                <el-table-column prop="API" label="API" sortable="custom"></el-table-column>
+                <el-table-column prop="Category" label="Category" sortable="custom"></el-table-column>
+                <el-table-column prop="Auth" label="Auth" sortable="custom"></el-table-column>
+                <el-table-column prop="Description" label="Description" sortable="custom"></el-table-column>
+                <el-table-column prop="HTTPS" label="HTTPS" sortable="custom"></el-table-column>
+            </data-tables>
+        </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      tableData: [],
+      actionColDef: {
+        label: "Link",
+        def: [
+          {
+            handler: row => {
+              window.open(row.Link);
+            },
+            name: "Go"
+          }
+        ]
+      },
+      paginationDef: {
+        show: false,
+        currentPage: 5
+      }
+    };
+  },
+  created: function() {
+    var that = this;
+    const jsonDataSourceURL =
+      "https://raw.githubusercontent.com/toddmotto/public-apis/master/json/entries.min.json";
+    // Use fetch API to grab json from git repo
+    fetch(jsonDataSourceURL)
+      .then(function(response) {
+        // Convert to JSON
+        return response.json();
+      })
+      .then(function(jsonData) {
+        // Transform data to work with Data Tables
+        jsonData.entries.map(function(row, index) {
+          // Overwrite Auth with "None" if value is null
+          if (row.Auth === null) {
+            jsonData.entries[index].Auth = "None";
+          }
+          // Overwrite HTTPS true/false values to "Yes/No"
+          jsonData.entries[index].HTTPS === true
+            ? (jsonData.entries[index].HTTPS = "Yes")
+            : (jsonData.entries[index].HTTPS = "No");
+        });
+        that.tableData = jsonData.entries;
+      });
+  },
+  methods: {
+    getActionsDef() {
+      let self = this;
+      return {
+        width: 5,
+        def: [
+          {
+            name: "Source Code",
+            handler() {
+              window.open('https://github.com/kschluter/api-browser');
+            },
+            icon: "plus"
+          }
+        ]
+      };
+    },
+    getCheckFilterDef() {
+      return {
+        width: 14,
+        props: "Auth",
+        def: [
+          {
+            code: "apiKey",
+            name: "apiKey"
+          },
+          {
+            code: "OAuth",
+            name: "OAuth"
+          },
+          {
+            code: "X-Mashape-Key",
+            name: "X-Mashape-Key"
+          },
+          {
+            code: "None",
+            name: "None"
+          }
+        ]
+      };
     }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: Source Sans Pro,Helvetica Neue,Arial,sans-serif;
 }
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.el-header{
+  height: 30px !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import DataTables from 'vue-data-tables'
+
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+
+locale.use(lang)
+
+Vue.use(Element)
+Vue.use(DataTables)
+
 new Vue({
   el: '#app',
   render: h => h(App)
